7885089084047a647616c4f70cc47ba52bd63153
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2024-01-12 21:21:07 +0000

    Updated frontend and cleaned up models

--- Timesheet/Models/Auth/Credentials.cs
+++ Timesheet/Models/Auth/Credentials.cs
@@ -1,44 +1,25 @@
-namespace Timesheet.Models.Auth
+namespace Timesheet.Models.Auth;
+
+public class Credentials
 {
-    public class Credentials
+
+    public string Token { get; set; } = string.Empty;
+    public bool Admin { get; set; }
+    public int Id { get; set; }
+
+    public Credentials()
     {
-        private string token = string.Empty;
-        private bool admin;
-        private int id;
+        // Jackson deserialization
+    }
 
-        public Credentials()
-        {
-            // Jackson deserialization
-        }
-
-        public Credentials(string token, bool admin)
-        {
-            this.token = token;
-            this.admin = admin;
-        }
-        
-
-        public string Token
-        {
-            get { return token; }
-            set { token = value; }
-        }
-
-        public bool Admin
-        {
-            get { return admin; }
-            set { admin = value; }
-        }
-
-        public int Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
-
-        public override string ToString()
-        {
-            return $"Credentials{{token='{token}', admin={admin}, id={id}}}";
-        }
+    public Credentials(string token, bool admin)
+    {
+        this.Token = token;
+        this.Admin = admin;
+    }
+    public override string ToString()
+    {
+        return $"Credentials{{token='{Token}', admin={Admin}, id={Id}}}";
     }
 }
+
